4e39ba2ca7db81bf028302e199e44fea18dfdd8d
Deng Junhui <120795714+minorcell@users.noreply.github.com>
2025-11-30 03:55:06 +0800

    Remove base URL from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(() => {
   return {
-    base: '/control-the-universe-with-your-hands/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
